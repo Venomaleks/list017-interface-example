c525e23456e8b36cfd7a395735574bfb97ee01c1
Venomaleks <aleks.edholm@gmail.com>
2021-03-09 11:52:03 +0100

    computer project and one interface, class

---
+++ src/Computer.java
@@ -0,0 +1,54 @@
+
+public class Computer implements ComputerOperastions{
+
+	private String className= "";
+
+	
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	
+	public String getClassName() {
+		return className;
+	}
+	
+	
+	public void turnOn() {
+		System.out.println(getClassName() + "is now turned on!");
+		
+		
+	}
+
+	
+	public void turnOff() {
+		System.out.println(getClassName() + "is now turned off!");
+		
+	}
+
+	
+	public void perfomDownload() {
+		System.out.println(getClassName() + "is now downloding a file!");
+		
+	}
+
+	
+	public void performUpload() {	
+		System.out.println(getClassName() + "is now upploding a file!");
+	
+	}
+
+	
+	public void playVideo() {
+		System.out.println(getClassName() + "is now playing a vudeo!");
+		
+	}
+
+	
+	public void playMusic() {
+		System.out.println(getClassName() + "is now playing a song!");
+		
+	}
+
+
+	
+}
